bc19b7a675f1288d6b949dd3ad6f2dd4d8b57f38
Bohdan Predchuk <predchukbohdan1995@ukr.net>
2025-07-15 00:13:12 +0300

    created swagger docs

--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import { UPLOAD_DIR } from './constants/index.js';
 import router from './routers/index.js';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 const PORT = Number(getEnvVar('PORT', '3000'));
 
@@ -25,6 +26,7 @@ export const setupServer = () => {
   );
 
   app.use('/uploads', express.static(UPLOAD_DIR));
+  app.use('/api-docs', swaggerDocs());
   app.use(router);
   app.use(notFoundHandler);
   app.use(errorHandler);
